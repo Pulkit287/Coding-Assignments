7664a87eceb383fdd26deb104942ee4363eaf605
Pulkit Mathur <pulkitmathur98@gmail.com>
2017-12-06 23:28:43 +0530

    Using temp variable

--- swap3.c
+++ swap3.c
@@ -1,13 +1,15 @@
 #include<stdio.h>
 void main()
 {
-int a,b,c;
+int a,b,temp;
 printf("Enter 2 Nos.\n");
 scanf("%d %d",&a,&b);
 printf("Old Values: \n a= %d & b= %d",a,b);
-c=a;
+//used temp variable
+//to swap values
+temp=a;
 a=b;
-b=c;
+b=temp;
 printf("\nNew Values: \n a= %d & b= %d  ",a,b);
 }
 
